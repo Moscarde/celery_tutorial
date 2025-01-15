fd4e18624c1dd9d78ca9222806fea50fd192e853
Gabriel Moscarde <gabrielmoscarde@outlook.com>
2025-01-15 20:17:45 -0300

    Add slow addition task with delay in Celery

--- tasks.py
+++ tasks.py
@@ -1,7 +1,14 @@
 from celery import Celery
+import time
 
-app = Celery("tasks", broker="pyamqp://guest@localhost//")
+app = Celery("tasks", backend="rpc://", broker="pyamqp://guest@localhost//")
+
 
 @app.task
 def add(x, y):
+    return x + y
+
+@app.task
+def add__slow(x, y):
+    time.sleep(15)
     return x + y
